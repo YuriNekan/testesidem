c6b72ec67d0d4ebba8a7bf6f13018990f0dca920
YuriNekan <email-que-usou-pra-criar-a-conta-do-github>
2019-07-25 18:30:06 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# testesidem git init git commit -m first commit git remote add origin https://github.com/YuriNekan/testesidem.git git push -u origin master
---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { InstalacaoPage } from './instalacao/instalacao.page';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'Usuários',
+      url: '/list',
+      icon: 'people'
+    },
+    {
+      title: 'Grupos',
+      url: '/Grupos',
+      icon: 'chatbubbles'
+    },
+    {
+      title: 'Instalação',
+      url: '/instalacao',
+      icon: 'home'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
